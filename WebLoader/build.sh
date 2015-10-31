e789440da7aea79a9ce0e07f83267914775506d1
terry <terry@rokid.com>
2015-11-01 01:05:40 +0800

    change loader to commons

--- WebLoader/build.sh
+++ WebLoader/build.sh
@@ -1,1 +1,1 @@
-javac -cp src/:lib/commons-codec-1.9.jar:lib/commons-logging-1.2.jar:lib/fluent-hc-4.5.1.jar:lib/httpclient-4.5.1.jar:lib/httpclient-cache-4.5.1.jar:lib/httpclient-win-4.5.1.jar:lib/httpcore-4.4.3.jar:lib/httpmime-4.5.1.jar:lib/jna-4.1.0.jar:lib/jna-platform-4.1.0.jar:lib/mysql-connector-java-5.1.25-bin.jar -d bin/ src/com/robot/AIApplication.java
+javac -cp src/:lib/commons-io-1.4.jar:lib/mysql-connector-java-5.1.25-bin.jar -d bin/ src/com/robot/AIApplication.java
--- WebLoader/src/com/robot/webloader/WebLoader.java
+++ WebLoader/src/com/robot/webloader/WebLoader.java
@@ -3,7 +3,10 @@ package com.robot.webloader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Created by terry on 15/10/31.
@@ -36,16 +39,32 @@ public class WebLoader implements Runnable {
 
     @Override
     public void run() {
-        Downloader downloader = new Downloader();
+//        Downloader downloader = new Downloader();
         try {
             while (is_run) {
                 URLModel target_url = urlQueue.get_one_url();
                 if (target_url != null && target_url.url.length() > 0) {
-//                    System.out.printf("URL : %s\n", target_url.url);
-                    String data = downloader.getHtml(target_url.url);
+                    System.out.printf("URL : %s\n", target_url.url);
+                    URL httpurl = null;
+                    try {
+                        httpurl = new URL(target_url.url);
+                    } catch (MalformedURLException e) {
+//                        e.printStackTrace();
+                        System.err.printf("target_url is not a good url!\n");
+                        continue;
+                    }
+//                    String data = downloader.getHtml(target_url.url);
                     // Save it
                     String file_name = target_url.url_id + ".html";
-                    save_web_data(data, "data/", file_name);
+                    File f = new File("data/" + file_name);
+                    try {
+                        FileUtils.copyURLToFile(httpurl, f);
+                    } catch (IOException e) {
+//                        e.printStackTrace();
+                        System.err.printf("url load err\n");
+                        continue;
+                    }
+//                    save_web_data(data, "data/", file_name);
                 } else {
                     System.out.println("No urls in DB");
                     Thread.sleep(1000);
